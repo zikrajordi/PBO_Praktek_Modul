89bcc44cce74dff8b6bfb522f798ea8fabd1f965
zikrajordi <114377127+zikrajordi@users.noreply.github.com>
2023-01-27 15:43:40 +0700

    Create latihan7.java

---
+++ zikra230922/latihan7.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zikra230922;
+
+/**
+ *
+ * @author user
+ */
+public class latihan7 {
+   public static void main( String[] args ){
+
+ boolean val1 = true;
+ boolean val2 = false;
+ System.out.println(!val1);
+ System.out.println(!val2);
+ }
+}
