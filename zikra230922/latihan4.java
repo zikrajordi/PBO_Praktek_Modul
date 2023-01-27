be64566b983831bc7997f8f3da8f0b8f36d9f6f0
zikrajordi <114377127+zikrajordi@users.noreply.github.com>
2023-01-27 15:43:29 +0700

    Create latihan4.java

---
+++ zikra230922/latihan4.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zikra230922;
+
+/**
+ *
+ * @author user
+ */
+public class latihan4 {
+    public static void main( String[] args ){
+
+ int i = 0;
+ int j = 10;
+ boolean test= false;
+
+ //demonstrasi &&
+ test = (i > 10) && (j++ > 9);
+ System.out.println(i);
+ System.out.println(j);
+ System.out.println(test);
+
+ //demonstrasi &
+ test = (i > 10) & (j++ > 9);
+ System.out.println(i);
+ System.out.println(j); 
+ System.out.println(test);
+ 
+ } 
+    
+}
