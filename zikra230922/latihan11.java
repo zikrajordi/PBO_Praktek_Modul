3e8695539a490b7cbd83593d28c2ecfb01f23f83
zikrajordi <114377127+zikrajordi@users.noreply.github.com>
2023-01-27 15:43:05 +0700

    Create latihan11.java

---
+++ zikra230922/latihan11.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zikra230922;
+
+/**
+ *
+ * @author user
+ */
+public class latihan11 {
+    public static void main(String[] args) {
+        int number1=10;
+        int number2=20;
+        int number3=45;
+        float avg;
+    
+        avg = (number1+number2+number3)/3;
+        
+        System.out.println("number 1 = " + number1);
+        System.out.println("number 2 = " + number2);
+        System.out.println("number 3 = " + number3);
+    }
+}
