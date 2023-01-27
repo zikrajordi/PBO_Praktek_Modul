6eb612b7e36f2dbdf52e7eafea6c9873d435e78e
zikrajordi <114377127+zikrajordi@users.noreply.github.com>
2023-01-27 15:43:36 +0700

    Create latihan6.java

---
+++ zikra230922/latihan6.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zikra230922;
+
+/**
+ *
+ * @author user
+ */
+public class latihan6 {
+   public static void main( String[] args ){
+
+ boolean val1 = true;
+ boolean val2 = true;
+ System.out.println(val1 ^ val2);
+ val1 = false;
+ val2 = true;
+ System.out.println(val1 ^ val2);
+
+ val1 = false;
+ val2 = false;
+ System.out.println(val1 ^ val2);
+ val1 = true;
+ val2 = false;
+ System.out.println(val1 ^ val2);
+ } 
+}
