eea7be01a657c86994a80357e4849a3a756826eb
zikrajordi <114377127+zikrajordi@users.noreply.github.com>
2023-01-27 15:43:54 +0700

    Create latihan1a.java

---
+++ zikra28102022/latihan1a.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zikra28102022;
+
+/**
+ *
+ * @author user
+ */
+public class latihan1a {
+    public static void main(String[] args) {
+        int i = 0;
+        String days[] = {"Senin","Selasa","Rabu","Kamis","Jum'at","Sabtu","Minggu"};
+        
+        while (i < 7){
+            System.out.println(days[i]);
+            i++;
+        }
+    }
+}
+
