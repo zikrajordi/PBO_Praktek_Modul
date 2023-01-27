8e4e333dd9fd66a55596fec496bb9ac3d5762f61
zikrajordi <114377127+zikrajordi@users.noreply.github.com>
2023-01-27 15:44:29 +0700

    Create latihan3.java

---
+++ zikra30092022/latihan3.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zikra30092022;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author user
+ */
+public class latihan3 {
+    public static void main( String[] args ){
+ String name = "";
+ name = JOptionPane.showInputDialog ("Please enter yourname");
+
+ String msg = "Hello " + name + "!";
+
+ JOptionPane.showMessageDialog(null, msg);
+
+ } 
+}
+
