4f6a581ea7ddc7a3a364e0e9d06a0a436e521f5a
zikrajordi <114377127+zikrajordi@users.noreply.github.com>
2023-01-27 15:43:44 +0700

    Create latihan8.java

---
+++ zikra230922/latihan8.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zikra230922;
+
+/**
+ *
+ * @author user
+ */
+public class latihan8 {
+    public static void main( String[] args ){
+
+ String status = "";
+ int grade = 80;
+
+ //mendapatkan status pelajar
+ status = (grade >= 60)?"Passed":"Fail";
+ //print status
+ System.out.println( status );
+ } 
+}
