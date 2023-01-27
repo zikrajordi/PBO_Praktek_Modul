8e2cb55fe1ea743172c39bb7bd217225e64ce7a5
zikrajordi <114377127+zikrajordi@users.noreply.github.com>
2023-01-27 15:42:51 +0700

    Create HelloWorld.java

---
+++ zikra160922/HelloWorld.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zikra160922;
+
+/**
+ *
+ * @author user
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World\n"+"MI2C"+"\nPemograman Berorientasi Objek\n");
+        System.out.println("Nama : Zikra Jordi\n"+"NoBp : 2101092074"+"\nProdi : Manajemen Informatika\n"
+        +"Jurusan : Teknologi Informasi\n"+"Alamat : Pesisir Selatan");
+       
+    }
+ 
+}
