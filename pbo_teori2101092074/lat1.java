8e5bbf24cae076973785dccccf7b606105aefcd7
zikrajordi <114377127+zikrajordi@users.noreply.github.com>
2023-01-27 15:42:06 +0700

    Create lat1.java

---
+++ pbo_teori2101092074/lat1.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo_teori2101092074;
+
+/**
+ *
+ * @author User
+ */
+public class lat1 {
+    
+}
